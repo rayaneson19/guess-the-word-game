206f110962e46060a94af0d96c01bc6127235736
Rayaneson19 <147350900+rayaneson19@users.noreply.github.com>
2023-10-13 22:28:31 +0100

    Update Wordle.py

--- #hangman.py
+++ #hangman.py
@@ -1,6 +1,9 @@
 #hangman
 
 import random
+import time
+
+
 secret_words = ["Ability", "Access", "Accident", "Account", "Act", "Action", "Activity", "Actor", "Ad", "Addition", "Address", "Administration", "Advantage", "Advertising", "Advice", "Affair", "Age", "Agency", "Agreement", "Air", "Airport", "Alcohol", "Ambition", "Amount", "Analysis", "Analyst", "Animal", "Answer", "Anxiety", "Apartment", "Appearance", "Apple", "Application", "Appointment", "Area", "Argument", "Army", "Arrival", "Art", "Article", "Aspect", "Assignment", "Assistance", "Assistant", "Association", "Assumption", "Atmosphere", "Attempt", "Attention", "Attitude", "Audience", "Aunt", "Average", "Awareness","Back", "Bad", "Balance", "Ball", "Bank", "Baseball", "Basis", "Basket", "Bath", "Bathroom", "Bedroom", "Beer", "Beginning", "Benefit", "Bird", "Birth", "Birthday", "Bit", "Blood", "Board", "Boat", "Body", "Bonus", "Book", "Boss", "Bottom", "Box", "Boy", "Boyfriend", "Bread", "Breath", "Brother", "Building", "Bus", "Business", "Buyer",
 "Cabinet", "Camera", "Cancer", "Candidate", "Capital", "Car", "Card", "Care", "Career", "Case", "Cash", "Cat", "Category", "Cause", "Celebration", "Cell", "Championship", "Chance", "Chapter", "Charity", "Cheek", "Chemistry", "Chest", "Chicken", "Child", "Childhood", "Chocolate", "Choice", "Church", "Cigarette", "City", "Class", "Classroom", "Client", "Climate", "Clothes", "Coast", "Coffee", "Collection", "College", "Combination", "Committee","communication","Dad", "Data", "Database", "Date", "Day", "Dealer", "Death", "Debt", "Decision", "Definition", "Delivery", "Demand", "Department", "Departure", "Depression", "Depth", "Description", "Design", "Desk", "Development", "Device", "Diamond", "Difference", "Difficulty", "Dinner", "Direction", "Director", "Dirt", "Disaster", "Discipline", "Discussion", "Disease", "Disk", "Distribution", "Dog", "Drama", "Drawer", "Drawing", "Driver",
 "Ear", "Earth", "Economics", "Economy", "Editor", "Education", "Effect", "Efficiency", "Effort", "Egg", "Election", "Elevator", "Emotion", "Emphasis", "Employee", "Employer", "Employment", "End", "Energy", "Engine", "Entertainment", "Enthusiasm", "Entry", "Environment", "Equipment", "Error", "Establishment", "Estate", "Event", "Exam", "Examination", "Example", "Exchange", "Excitement", "Exercise", "Experience", "Explanation", "Expression", "Extent", "Eye", "Face", "Fact", "Failure", "Family", "Farmer", "Fat", "Feature", "Feedback", "Field", "Figure", "Film", "Finding", "Fire", "Fish", "Flight", "Focus", "Food", "Football", "Force", "Form", "Fortune", "Foundation", "Frame", "Freedom", "Friendship", "Fun", "Funeral", "Future", "Game", "Garbage", "Garden", "Gate", "Gene", "Gift", "Girl", "Girlfriend", "Goal", "Government", "Grandmother", "Grocery", "Group", "Growth", "Guest", "Guidance", "Guide", "Guitar","Hair", "Half", "Hall", "Hand", "Hat", "Head", "Health", "Hearing", "Heart", "Heat", "Height", "Highway", "Historian", "History", "Home", "Homework", "Honey", "Hope", "Hospital", "Hotel", "House", "Housing",
@@ -17,18 +20,34 @@ v = []
 win = True
 g_limit = 5
 guesses = 0
-s_word = random.choice(secret_words)
+s_word = random.choice(secret_words).lower()
 len_s_word = len(s_word)
 pos = len_s_word
 
+def m(string):
+   
+    for q in string:
+       
+        # printing each character of the message
+        print(q, end="", flush = True)
+         
+        # adding time delay of half second
+        time.sleep(0.05)
 for i in range(len_s_word):
     v.append('_')
 
-n = input(' \n  \n  \n  \n Hi. What is your name?                               ')
 
-print("""Hi """ + n + """. Lets play Hangman, but first we have to explain the game and its instructions so follow me:
+
+
+print(' \n  \n  \n  \n ')
+
+n = '      Hi. What is your name?                               '
+m(n)
+name = input('')
+print(' \n  \n  \n  \n ')
+msg = """Hi """ + name + """. Lets play Hangman, but first we have to explain the game and its instructions so follow me:
       
-      • You will have a blanked word. something like this '_ _ _'. And you need to guess the word.    
+      • You will have a blanked word. something like this '_ _ _'. And you need to guess the word.
       • You will have only 5 guesses before you die.
       • Every letter you get right will be shown.
       • You can only type a word that has the same number of letter the blank has. Example:
@@ -36,7 +55,8 @@ print("""Hi """ + n + """. Lets play Hangman, but first we have to explain the g
       
             ➡ Here is you word. Goodluck:
       
-""")
+"""
+m(msg)
 
 print('            _____________ ')
 print('            |          |')
@@ -60,16 +80,17 @@ print("                                      ", end="")
 print(*v, sep=" ")
 
 
-print(s_word)
 
 try:
     while (win) and guesses != g_limit:
-        user_inp = list(input(' \n   \n        ►  '))
+        user_inp = list(input(' \n   \n        ►  ').lower())
         if user_inp == list(s_word):
                 print(' \n  \n ')
                 print("                                      ", end="")
                 print(*v, sep="   ")
-                print(' \n  \n  \n ➡  Exellent job. You guessed my word right!')  
+                print(' \n  \n  \n ')
+                winn = ('➡  Exellent job. You guessed my word right!')  
+                m(winn)
                 win = False 
         for pos in range(len_s_word):
             if user_inp[pos] == s_word[pos]:
@@ -77,7 +98,8 @@ try:
             else:
                 print('')
         if (user_inp[pos] == s_word[pos] or user_inp[pos] != s_word[pos]) and user_inp != list(s_word):
-                print('             ➡  You just lost a guess.')
+                losee = (' ➡  You just lost a guess.')
+
                 guesses +=1
 
         print(' \n  \n ')
@@ -125,12 +147,12 @@ try:
                     print('            |           ')
                     print('           _|____________')
 
-                    print('   \n  \n                     Unfortunatly you lost the game. My word was '+ s_word)
+                    print('   \n  \n Unfortunatly you lost the game. My word was '+ s_word)
                     win = False
 
 except IndexError:
         print(' \n  \n  \n                 Type a word with the corresponding amount of letters. \n  \n  \n')
-        win = False
+
 
 
 
